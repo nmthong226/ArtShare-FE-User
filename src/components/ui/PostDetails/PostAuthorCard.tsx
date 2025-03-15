a3f6941eb04e7e1139e995085edeaf43990fffe8
Phan Trương Quý Hòa <93481453+quyhoaphantruong@users.noreply.github.com>
2025-03-15 22:11:21 +0700

    setup for PostDetails

---
+++ src/components/ui/PostDetails/PostAuthorCard.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface PostAuthorCardProps {
+    authorName: string;
+    authorAvatar: string;
+    postDate: string;
+}
+
+const PostAuthorCard: React.FC<PostAuthorCardProps> = ({ authorName, authorAvatar, postDate }) => {
+    return (
+        <div className="post-author-card">
+            <img src={authorAvatar} alt={`${authorName}'s avatar`} className="author-avatar" />
+            <div className="author-details">
+                <h3 className="author-name">{authorName}</h3>
+                <p className="post-date">{postDate}</p>
+            </div>
+        </div>
+    );
+};
+
+export default PostAuthorCard;

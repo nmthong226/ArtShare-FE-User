01302a376e30e076cb96ecce42dcaf240113876b
nmthong226 <nmthong226@gmail.com>
2025-04-12 23:34:29 +0700

    style: add more details for art gen

--- src/features/gen-art/ArtGenAI.tsx
+++ src/features/gen-art/ArtGenAI.tsx
@@ -14,17 +14,20 @@ import { GoSidebarExpand } from "react-icons/go";
 import { IoImageOutline } from "react-icons/io5";
 import { IoIosArrowForward } from "react-icons/io";
 import { IoIosSquareOutline } from "react-icons/io";
-import { VscSymbolColor } from "react-icons/vsc";
 import { GoLightBulb } from "react-icons/go";
 import { AiOutlineCamera } from "react-icons/ai";
 import { IoColorFillOutline } from "react-icons/io5";
+import { TbChessQueenFilled } from "react-icons/tb";
+import { PiStarFourFill } from "react-icons/pi";
+import { IoMdArrowDropdown } from "react-icons/io";
 
 //Assets
 import example_1 from "./assets/1.webp"
+import { Input } from '@/components/ui/input';
 
 const ArtGenAI = () => {
-    const [numberOfImages, setNumberOfImages] = useState(1);
-    const [expanded, setExpanded] = useState<boolean>(false);
+    const [numberOfImages, setNumberOfImages] = useState('4');
+    const [expanded, setExpanded] = useState<boolean>(true);
 
     const handleParentToggle = (_event: React.SyntheticEvent, isExpanded: boolean) => {
         setExpanded(isExpanded);
@@ -32,7 +35,7 @@ const ArtGenAI = () => {
 
     const handleSelectNumber = (
         _: React.MouseEvent<HTMLElement>,
-        newNumber: number
+        newNumber: string
     ) => {
         if (newNumber) {
             setNumberOfImages(newNumber);
@@ -58,7 +61,7 @@ const ArtGenAI = () => {
                 </AccordionSummary>
                 <AccordionDetails className='flex flex-col p-0 h-[600px] overflow-y-auto custom-scrollbar'>
                     {/* Nested Accordions */}
-                    <Accordion className="shadow-none">
+                    <Accordion className="shadow-none" defaultExpanded>
                         <AccordionSummary
                             expandIcon={<ArrowDropDownIcon />}
                             aria-controls="panel2-content"
@@ -91,7 +94,7 @@ const ArtGenAI = () => {
                             </div>
                         </AccordionDetails>
                     </Accordion>
-                    <Accordion className="shadow-none">
+                    <Accordion className="shadow-none" defaultExpanded>
                         <AccordionSummary
                             expandIcon={<ArrowDropDownIcon />}
                             aria-controls="panel2-content"
@@ -103,7 +106,6 @@ const ArtGenAI = () => {
                         </AccordionSummary>
                         <AccordionDetails>
                             <div className='flex flex-col space-y-1'>
-                                <p className='text-mountain-600 text-sm'>Image Effect</p>
                                 <Button className='flex justify-between bg-mountain-100 p-3 rounded-xl w-full font-normal'>
                                     <div className='flex items-center space-x-2'>
                                         <IoColorFillOutline className='rounded-xs w-5 h-5' />
@@ -128,7 +130,7 @@ const ArtGenAI = () => {
                             </div>
                         </AccordionDetails>
                     </Accordion>
-                    <Accordion className="shadow-none">
+                    <Accordion className="shadow-none" defaultExpanded>
                         <AccordionSummary
                             expandIcon={<ArrowDropDownIcon />}
                             aria-controls="panel2-content"
@@ -188,7 +190,7 @@ const ArtGenAI = () => {
                                     </ToggleButton>
                                     <ToggleButton
                                         value="3"
-                                        className="-m-0.5 px-4 py-2 border-0 rounded-full w-1/4 normal-case transition duration-300 ease-in-out transform"
+                                        className="-m-0.5 px-4 py-2 border-0 rounded-full w-1/5 normal-case transition duration-300 ease-in-out transform"
                                         sx={{
                                             backgroundColor: '#e0e0e0',
                                             '&.Mui-selected': {
@@ -230,7 +232,32 @@ const ArtGenAI = () => {
                     </Accordion>
                 </AccordionDetails>
             </Accordion>
-            <div className='flex bg-mountain-100 w-3/4'>b</div>
+            <div className='relative flex flex-col items-end rounded-xl w-4/5'>
+                <div className='flex items-center space-x-2 bg-white shadow-md p-2 rounded-xl w-fit h-13'>
+                    <div className='flex h-full'>
+                        <div className='flex justify-center items-center bg-mountain-100 px-2 rounded-lg w-fit h-full font-normal'>
+                            <div className='flex items-center space-x-2'>
+                                <p>Generate <span className='font-medium'>Image</span></p>
+                                <IoMdArrowDropdown />
+                            </div>
+                        </div>
+                    </div>
+                    <div className='flex px-2 border-mountain-100 border-r-2 border-l-2 h-full'>
+                        <div className='flex justify-center items-center space-x-2 bg-mountain-100 px-2 rounded-lg w-fit h-full font-normal'>
+                            <p>50</p>
+                            <PiStarFourFill className='size-5' />
+                        </div>
+                    </div>
+                    <Button className='flex justify-center items-center bg-indigo-100 rounded-lg w-28 h-full font-normal'>
+                        <TbChessQueenFilled className='mr-2 size-5' />
+                        <p>Upgrade</p>
+                    </Button>
+                </div>
+                <div className='bottom-2 left-1/2 absolute flex -translate-x-1/2'>
+                    <Input placeholder='What do you imagine about?...' className='relative flex bg-white shadow-md pr-28 border border-mountain-100 rounded-xl w-[760px] h-15 placeholder:text-mountain-400' />
+                    <Button className='top-1/2 right-4 absolute flex items-center bg-indigo-100 px-4 -translate-y-1/2'>Generate</Button>
+                </div>
+            </div>
         </div>
     )
 }

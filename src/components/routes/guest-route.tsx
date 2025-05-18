f7a4bcc781a096f1916cd805bddbcea014e7770f
Kiet <kietnguyentuan911@gmail.com>
2025-05-18 17:44:56 +0700

    chores: remove redundant comments

--- src/components/routes/guest-route.tsx
+++ src/components/routes/guest-route.tsx
@@ -1,4 +1,3 @@
-// src/components/ProtectedItems/GuestRoute.tsx
 import React from "react"
 import { Navigate, useLocation } from "react-router-dom"
 import { useUser } from "@/contexts/UserProvider"

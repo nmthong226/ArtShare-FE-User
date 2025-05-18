e584ea29774faa797dce1703e41a2bc68508bec6
Huy Luu <luudinhhuy2003@gmail.com>
2025-05-18 15:58:03 +0700

    add test-sm to classname

--- src/pages/Onboarding/index.tsx
+++ src/pages/Onboarding/index.tsx
@@ -237,7 +237,7 @@ const OnboardingProfile: React.FC = () => {
               id="bio"
               minRows={3}
               maxLength={150}
-              className="w-full px-4 py-2 rounded-lg border border-neutral-300 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white text-neutral-900"
+              className="w-full px-4 py-2 rounded-lg border border-neutral-300 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white text-neutral-900 text-sm"
               placeholder="A short description about you"
               {...register("bio")}
               style={{ resize: "none" }}
